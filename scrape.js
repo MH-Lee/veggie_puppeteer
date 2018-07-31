15a94f6d3561d60a234fe7085f915f7a72f34912
MH-Lee <lmhoon012@gmail.com>
2018-08-01 05:38:52 +0900

    puppeteer code 수정

--- scrape.js
+++ scrape.js
@@ -1,45 +1,36 @@
 const axios = require('axios');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-const { setCookie, getCookie } = require('./cookie.js');
 
 const words = fs.readFileSync('./wanted_url.txt', 'utf8');
 const arr = words.split(',');
 
 // url
-const jwtURL = 'http://127.0.0.1:8000/api/v1/accounts/api-token-auth/';
-const contentURL = 'http://127.0.0.1:8000/api/v1/contents/job_contents/';
+const jwtURL = 'http://45.77.179.168:3000/api/v1/accounts/api-token-auth/';
+const contentURL = 'http://45.77.179.168:3000/api/v1/contents/job_contents/';
 
 // Authorization
 const username = 'test';
 const password = 'test123123123';
 
-const scrapeData = async (url) => {
-  // Set up browser and page.
-  const browser = await puppeteer.launch({
-    headless: false,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  });
-  const page = await browser.newPage();
-  page.setViewport({ width: 1280, height: 926 });
-  await page.goto(url);
-  // post data
-  const postData = async (contentData) => {
-    const jwtData = {
-      username,
-      password,
-    };
-    const jwtToken = await axios.post(jwtURL, jwtData);
-    const token = jwtToken.data.token;
-    // 위에서 받은 토큰을 브라우저 쿠키에 저장한다
-    document.cookie = setCookie('VA-TOKEN', token);
-    // 쿠키를 가져와서 토큰을 사용한다
-    const vaToken = getCookie('VA-TOKEN');
-    const headerData = {
-      Authorization: `JWT ${vaToken}`,
-    };
-    await axios.post(contentURL, contentData, { headers: headerData });
+// post data
+const postData = async (contentData) => {
+  const jwtData = {
+    username,
+    password,
   };
+  const jwtToken = await axios.post(jwtURL, jwtData);
+  const token = jwtToken.data.token;
+  // 쿠키를 가져와서 토큰을 사용한다
+  const headerData = {
+    Authorization: `JWT ${token}`,
+  };
+  const res = await axios.post(contentURL, contentData, { headers: headerData });
+  return res;
+};
+
+const scrapeData = async (page, url) => {
+  await page.goto(url);
 
   const data = await page.evaluate(() => {
     const companyJobtitle = document.querySelectorAll('#app > main > div:nth-child(1) > div:nth-child(2) > div.react-container > div.UserJobDetail-cCsxOA.kySZNS > div > div > div > div:nth-child(2) > div.Div-hTZHGu.fezPpG > div:nth-child(2) > h3');
@@ -59,16 +50,27 @@ const scrapeData = async (url) => {
     console.log(title, company, location);
     return contentData;
   });
-  await postData(data);
+  const res = await postData(data);
+  return res;
 };
 
 
 const main = async () => {
+  // Set up browser and page.
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+  const page = await browser.newPage();
+  page.setViewport({ width: 1280, height: 926 });
+
   for (let i = 0; i < arr.length; i++) {
     console.log(arr[i]);
-    const contentData = await scrapeData(arr[i]);
+    const contentData = await scrapeData(page, arr[i]);
     console.log(contentData);
   }
+
+  await browser.close();
 };
 
 main();
